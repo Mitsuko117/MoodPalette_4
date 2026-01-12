6c2fa84d956077b1db1659d203dffa2b65e671f3
Mitsuko117 <mitsuko.nishi@gmail.com>
2026-01-12 19:33:28 +0900

    Add files via upload

---
+++ db_config_keyなし.php
@@ -0,0 +1,10 @@
+<?php
+// /home/アカウント名/config/db_config.php
+
+// データベース接続情報
+define('DB_NAME', 'XXXXXXXXXXXXXXXXXXXXXX');
+define('DB_HOST', 'XXXXXXXXXXXXXXXXXXXXXX');  
+define('DB_USER', 'XXXXXXXXXXXXXXXXXXXXXX');
+define('DB_PASS', 'XXXXXXXXXXXXXXXXXXXXXX');
+
+?>
